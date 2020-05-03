6b9e35bf1c4f049de8cd6e8f8d921f372f468e31
Mike-Fernandez <00145518@uca.edu.sv>
2020-05-03 16:21:06 -0600

    Modifying the action bar

--- app/src/main/java/com/programoviles/sweetmusic/MainActivity.kt
+++ app/src/main/java/com/programoviles/sweetmusic/MainActivity.kt
@@ -9,9 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val actionBar: ActionBar? = supportActionBar
-        supportActionBar?.setDisplayShowHomeEnabled(true);
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.action_bar_layout)
+
+//        val actionBar: ActionBar? = supportActionBar
+/*        supportActionBar?.setDisplayShowHomeEnabled(true);
         supportActionBar?.setLogo(R.mipmap.ic_launcher);
-        supportActionBar?.setDisplayUseLogoEnabled(true);
+        supportActionBar?.setDisplayUseLogoEnabled(true);*/
     }
 }

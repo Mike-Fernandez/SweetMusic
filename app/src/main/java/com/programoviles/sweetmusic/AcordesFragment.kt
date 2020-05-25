de39a96176cb6847a638d4c934faf256cf450871
Mike-Fernandez <00145518@uca.edu.sv>
2020-05-24 19:28:12 -0600

    Added view for acordes tool

--- app/src/main/java/com/programoviles/sweetmusic/AcordesFragment.kt
+++ app/src/main/java/com/programoviles/sweetmusic/AcordesFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.databinding.DataBindingUtil
+import com.programoviles.sweetmusic.databinding.FragmentAcordesBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,8 +19,19 @@ class AcordesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_acordes, container, false)
+        val binding = DataBindingUtil.inflate<FragmentAcordesBinding>(inflater, R.layout.fragment_acordes,
+            container, false)
+
+//        val spinnerAcordes = binding.spChordName
+        val spinnerAcordes: Spinner = binding.spChordName
+        this.context?.let {
+            ArrayAdapter.createFromResource(it, R.array.acordes_array, android.R.layout.simple_spinner_item)
+                .also { adapter->
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    spinnerAcordes.adapter = adapter
+                }
+        }
+        return binding.root
     }
 
 }

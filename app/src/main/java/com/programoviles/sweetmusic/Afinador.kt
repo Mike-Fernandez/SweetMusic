c6ee62ddbd9747ba8077a8b03504e0621de3de3c
Julio Machado <00039178@uca.edu.sv>
2020-05-26 17:39:24 -0600

    added afinador navigation

--- app/src/main/java/com/programoviles/sweetmusic/Afinador.kt
+++ app/src/main/java/com/programoviles/sweetmusic/Afinador.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.programoviles.sweetmusic.databinding.FragmentAcordesBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +19,22 @@ class Afinador : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_afinador, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentAcordesBinding>(inflater, R.layout.fragment_acordes,
+        container, false)
+
+        binding.toolbarAcordesMenu.setOnNavigationItemReselectedListener {
+            when(it.itemId) {
+                R.id.acordes_access_afinador -> findNavController().navigate(R.id.action_afinador_to_acordesFragment)
+                R.id.glosario_access_afinador -> findNavController().navigate(R.id.action_afinador_to_glosarioFragment)
+                R.id.metronomo_access_afinador -> findNavController().navigate(R.id.action_afinador_to_metronomo)
+                R.id.home_access_afinador -> findNavController().navigate(R.id.action_afinador_to_mainMenuFragment)
+            }
+        }
+
+        return binding.root
+
+        //return inflater.inflate(R.layout.fragment_afinador, container, false)
     }
 
 }

2b151a833bc08456cf50d2b199524c20f5da73a0
SaulO1999 <54159308+SaulO1999@users.noreply.github.com>
2020-05-26 16:56:11 -0600

    Added navigation to metronomo

--- app/src/main/java/com/programoviles/sweetmusic/metronomo.kt
+++ app/src/main/java/com/programoviles/sweetmusic/metronomo.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.programoviles.sweetmusic.databinding.FragmentMetronomoBinding
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class metronomo : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_metronomo, container, false)
+        val binding = DataBindingUtil.inflate<FragmentMetronomoBinding>(inflater,
+            R.layout.fragment_metronomo,container,false)
+
+        binding.toolbarMenuMetronomo.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.Home_access_metronomo -> this.findNavController().
+                    navigate(R.id.action_metronomo_to_mainMenuFragment)
+                R.id.glosario_access_metronomo -> this.findNavController().
+                    navigate(R.id.action_metronomo_to_glosarioFragment)
+                R.id.Afinador_access_metronomo -> this.findNavController().
+                    navigate(R.id.action_metronomo_to_afinador)
+                R.id.Acordes_access_metronomo -> this.findNavController().
+                    navigate(R.id.action_metronomo_to_acordesFragment)
+            }
+        }
+
+        return binding.root
     }
 
 }
